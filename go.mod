39acb43e536019ffc0ac36165229b7572c042696
Paweł Kopiczko <kopiczko@gmail.com>
2019-10-10 14:58:01 +0100

    gitrepo: initial implementaiton

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/giantswarm/gitrepo
 
 go 1.13
 
-require gopkg.in/src-d/go-billy.v4 v4.3.2
+require (
+	github.com/giantswarm/microerror v0.0.0-20190815145748-cb07ec533b50
+	github.com/go-errors/errors v1.0.1
+	github.com/juju/errgo v0.0.0-20140925100237-08cceb5d0b53 // indirect
+	gopkg.in/src-d/go-billy.v4 v4.3.2
+	gopkg.in/src-d/go-git.v4 v4.13.1
+)
--- pkg/gitrepo/repo.go
+++ pkg/gitrepo/repo.go
@@ -2,8 +2,18 @@ package gitrepo
 
 import (
 	"context"
+	"io"
+	"log"
 
-	"gopkg.in/src-d/go-billy.v4"
+	"github.com/giantswarm/microerror"
+	"github.com/go-errors/errors"
+	"gopkg.in/src-d/go-billy.v4/osfs"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/cache"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
+	"gopkg.in/src-d/go-git.v4/storage/filesystem"
 )
 
 type Config struct {
@@ -13,22 +23,138 @@ type Config struct {
 }
 
 type Repo struct {
-	authBasicToken string
-	url            string
+	url string
 
-	fs billy.Filesystem
+	auth    *http.BasicAuth
+	storage *filesystem.Storage
 }
 
 func New(config Config) (*Repo, error) {
-	r := &Repo{}
+	if config.AuthBasicToken == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.AuthBasicToken must not be empty", config)
+	}
+	if config.Dir == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Dir must not be empty", config)
+	}
+	if config.URL == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.URL must not be empty", config)
+	}
+
+	fs := osfs.New(config.Dir)
+	storage := filesystem.NewStorageWithOptions(fs, cache.NewObjectLRUDefault(), filesystem.Options{ExclusiveAccess: true})
+
+	r := &Repo{
+		url: config.URL,
+
+		auth: &http.BasicAuth{
+			Username: "can-be-anything-but-not-empty",
+			Password: config.AuthBasicToken,
+		},
+		storage: storage,
+	}
 
 	return r, nil
 }
 
 func (r *Repo) EnsureUpToDate(ctx context.Context) error {
+	cloneOpts := &git.CloneOptions{
+		Auth:       r.auth,
+		URL:        r.url,
+		NoCheckout: true,
+	}
+
+	repo, err := git.Clone(r.storage, nil, cloneOpts)
+	if errors.Is(err, git.ErrRepositoryAlreadyExists) {
+		repo, err = git.Open(r.storage, nil)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	} else if err != nil {
+		return microerror.Mask(err)
+	}
+
+	fetchOpts := &git.FetchOptions{
+		Auth:  r.auth,
+		Force: true,
+	}
+
+	err = repo.Fetch(fetchOpts)
+	if errors.Is(err, git.NoErrAlreadyUpToDate) {
+		// Fall trough.
+	} else if err != nil {
+		return microerror.Mask(err)
+	}
+
 	return nil
 }
 
 func (r *Repo) ResolveVersion(ctx context.Context, ref string) (string, error) {
-	return "", nil
+	repo, err := git.Open(r.storage, nil)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	tags := map[string]string{}
+	{
+		tagsIter, err := repo.Tags()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer tagsIter.Close()
+		for {
+			tag, err := tagsIter.Next()
+			if errors.Is(err, io.EOF) {
+				break
+			} else if err != nil {
+				log.Fatal(err)
+			}
+			tags[tag.Hash().String()] = tag.Name().Short()
+		}
+	}
+
+	var commit *object.Commit
+	{
+		hash, err := repo.ResolveRevision(plumbing.Revision(ref))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		commit, err = repo.CommitObject(*hash)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// When the commit is tagged return the tag.
+	tag, ok := tags[commit.Hash.String()]
+	if ok {
+		return tag, nil
+	}
+
+	// Otherwise find the first tagged parent and return it's tag glued
+	// with the SHA.
+	var version string
+	{
+		var lastTag string
+
+		c := commit
+		for {
+			tag, ok := tags[c.Hash.String()]
+			if ok {
+				lastTag = tag
+				break
+			}
+			c, err = c.Parent(0)
+			if errors.Is(err, object.ErrParentNotFound) {
+				lastTag = "v0.0.0"
+				break
+			} else if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		version = lastTag + "-" + commit.Hash.String()
+	}
+
+	return version, nil
 }
