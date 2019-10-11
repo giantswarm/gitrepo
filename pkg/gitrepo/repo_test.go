f770dfbcdfa214b1214dbfadb63026d690926e54
Theo Brigitte <theo.brigitte@gmail.com>
2019-10-11 13:46:20 +0200

    add test names

--- pkg/gitrepo/repo_test.go
+++ pkg/gitrepo/repo_test.go
@@ -8,31 +8,32 @@ import (
 // TestResolveVersion tests ResolveVersion method which resolve a git reference and find the project version for it.
 func TestResolveVersion(t *testing.T) {
 	testCases := []struct {
+		name            string
 		ref             string
 		expectedVersion string
 	}{
 		{
-			// untagged version
+			name:            "untagged version",
 			ref:             "2e7604b8b3806b20ff305eb4e1a852c784ba34ca",
 			expectedVersion: "v0.0.0-2e7604b8b3806b20ff305eb4e1a852c784ba34ca",
 		},
 		{
-			// tagged version
+			name:            "tagged version",
 			ref:             "d1dcd7e42b044858f14ad51ea68e2809c16deb84",
 			expectedVersion: "test-tag",
 		},
 		{
-			// above tagged version
+			name:            "above tagged version",
 			ref:             "b62b39c5f762eae26979715599a0a9226547ef5e",
 			expectedVersion: "test-tag-b62b39c5f762eae26979715599a0a9226547ef5e",
 		},
 		{
-			// branch reference
+			name:            "branch reference",
 			ref:             "test-branch",
 			expectedVersion: "test-tag",
 		},
 		{
-			// tag reference
+			name:            "tag reference",
 			ref:             "test-tag",
 			expectedVersion: "test-tag",
 		},
