b62b39c5f762eae26979715599a0a9226547ef5e
Paweł Kopiczko <pawel@giantswarm.io>
2019-10-11 12:37:10 +0100

    gitrepo: initial implementaiton (#2)
    Towards: https://github.com/giantswarm/giantswarm/issues/6844
    
    This PR implements the core logic of this library, which is to determine a project version based on a git reference like the `architect project version` command.
    The reason we need this is because we need to be able to determine version in other places like `opsctl`, so we need a re-usable piece of code.

--- pkg/gitrepo/repo.go
+++ pkg/gitrepo/repo.go
@@ -2,8 +2,18 @@ package gitrepo
 
 import (
 	"context"
+	"io"
 
-	"gopkg.in/src-d/go-billy.v4"
+	"github.com/giantswarm/microerror"
+	"github.com/go-errors/errors"
+	"gopkg.in/src-d/go-billy.v4/osfs"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/cache"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
+	"gopkg.in/src-d/go-git.v4/storage/filesystem"
 )
 
 type Config struct {
@@ -13,22 +23,145 @@ type Config struct {
 }
 
 type Repo struct {
-	authBasicToken string
-	url            string
+	url string
 
-	fs billy.Filesystem
+	auth    transport.AuthMethod
+	storage *filesystem.Storage
 }
 
 func New(config Config) (*Repo, error) {
-	r := &Repo{}
+	if config.Dir == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Dir must not be empty", config)
+	}
+	if config.URL == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.URL must not be empty", config)
+	}
+
+	var auth transport.AuthMethod
+	{
+		if config.AuthBasicToken != "" {
+			auth = &http.BasicAuth{
+				Username: "can-be-anything-but-not-empty",
+				Password: config.AuthBasicToken,
+			}
+		}
+	}
+
+	fs := osfs.New(config.Dir)
+	storage := filesystem.NewStorageWithOptions(fs, cache.NewObjectLRUDefault(), filesystem.Options{ExclusiveAccess: true})
+
+	r := &Repo{
+		url: config.URL,
+
+		auth:    auth,
+		storage: storage,
+	}
 
 	return r, nil
 }
 
 func (r *Repo) EnsureUpToDate(ctx context.Context) error {
+	cloneOpts := &git.CloneOptions{
+		Auth:       r.auth,
+		URL:        r.url,
+		NoCheckout: true,
+	}
+
+	repo, err := git.Clone(r.storage, nil, cloneOpts)
+	if errors.Is(err, git.ErrRepositoryAlreadyExists) {
+		repo, err = git.Open(r.storage, nil)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	} else if err != nil {
+		return microerror.Mask(err)
+	}
+
+	fetchOpts := &git.FetchOptions{
+		Auth:  r.auth,
+		Force: true,
+	}
+
+	err = repo.Fetch(fetchOpts)
+	if errors.Is(err, git.NoErrAlreadyUpToDate) {
+		// Fall through.
+	} else if err != nil {
+		return microerror.Mask(err)
+	}
+
 	return nil
 }
 
 func (r *Repo) ResolveVersion(ctx context.Context, ref string) (string, error) {
-	return "", nil
+	repo, err := git.Open(r.storage, nil)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	tags := map[string]string{}
+	{
+		tagsIter, err := repo.Tags()
+		if err != nil {
+			return "", microerror.Mask(err)
+		}
+		defer tagsIter.Close()
+		for {
+			tag, err := tagsIter.Next()
+			if errors.Is(err, io.EOF) {
+				break
+			} else if err != nil {
+				return "", microerror.Mask(err)
+			}
+			tags[tag.Hash().String()] = tag.Name().Short()
+		}
+	}
+
+	var commit *object.Commit
+	{
+		hash, err := repo.ResolveRevision(plumbing.Revision("origin/" + ref))
+		if err != nil {
+			hash, err = repo.ResolveRevision(plumbing.Revision(ref))
+			if err != nil {
+				return "", microerror.Mask(err)
+			}
+		}
+
+		commit, err = repo.CommitObject(*hash)
+		if err != nil {
+			return "", microerror.Mask(err)
+		}
+	}
+
+	// When the commit is tagged return the tag.
+	tag, ok := tags[commit.Hash.String()]
+	if ok {
+		return tag, nil
+	}
+
+	// Otherwise find the first tagged parent and return it's tag glued
+	// with the SHA.
+	var version string
+	{
+		var lastTag string
+
+		c := commit
+		for {
+			tag, ok := tags[c.Hash.String()]
+			if ok {
+				lastTag = tag
+				break
+			}
+			c, err = c.Parent(0)
+			if errors.Is(err, object.ErrParentNotFound) {
+				lastTag = "v0.0.0"
+				break
+			} else if err != nil {
+				return "", microerror.Mask(err)
+			}
+		}
+
+		version = lastTag + "-" + commit.Hash.String()
+	}
+
+	return version, nil
 }
