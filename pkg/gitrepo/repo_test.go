96696d1e4ca2113da4d92fe7f90532e317172f17
Theo Brigitte <theo.brigitte@gmail.com>
2019-10-11 15:37:58 +0200

    update test tag to 0.1.0

--- pkg/gitrepo/repo_test.go
+++ pkg/gitrepo/repo_test.go
@@ -21,22 +21,22 @@ func TestResolveVersion(t *testing.T) {
 		{
 			name:            "case 1: tagged version",
 			inputRef:        "d1dcd7e42b044858f14ad51ea68e2809c16deb84",
-			expectedVersion: "test-tag",
+			expectedVersion: "0.1.0",
 		},
 		{
 			name:            "case 2: above tagged version",
 			inputRef:        "b62b39c5f762eae26979715599a0a9226547ef5e",
-			expectedVersion: "test-tag-b62b39c5f762eae26979715599a0a9226547ef5e",
+			expectedVersion: "0.1.0-b62b39c5f762eae26979715599a0a9226547ef5e",
 		},
 		{
 			name:            "case 3: branch reference",
 			inputRef:        "test-branch",
-			expectedVersion: "test-tag",
+			expectedVersion: "0.1.0",
 		},
 		{
 			name:            "case 4: tag reference",
-			inputRef:        "test-tag",
-			expectedVersion: "test-tag",
+			inputRef:        "v0.1.0",
+			expectedVersion: "0.1.0",
 		},
 	}
 
