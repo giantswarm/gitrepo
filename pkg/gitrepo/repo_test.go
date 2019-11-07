b68520ad7ce02db42f10b0fbbccb6e22925087e1
Paweł Kopiczko <kopiczko@gmail.com>
2019-11-07 14:43:46 +0000

    failing tests

--- pkg/gitrepo/repo_test.go
+++ pkg/gitrepo/repo_test.go
@@ -2,11 +2,16 @@ package gitrepo
 
 import (
 	"context"
+	"os"
 	"strconv"
 	"testing"
 )
 
-// TestResolveVersion tests ResolveVersion method which resolve a git reference and find the project version for it.
+// TestResolveVersion tests ResolveVersion method which resolve a git reference
+// and find the project version for it. Tested repository can be found here:
+//
+//	https://github.com/giantswarm/gitrepo-test.
+//
 func TestResolveVersion(t *testing.T) {
 	testCases := []struct {
 		name            string
@@ -14,35 +19,63 @@ func TestResolveVersion(t *testing.T) {
 		expectedVersion string
 	}{
 		{
-			name:            "case 0: tag reference",
+			name:            "case 0: version tag",
 			inputRef:        "v0.1.0",
 			expectedVersion: "0.1.0",
 		},
 		{
-			name:            "case 1: untagged version",
-			inputRef:        "2e7604b8b3806b20ff305eb4e1a852c784ba34ca",
-			expectedVersion: "0.0.0-2e7604b8b3806b20ff305eb4e1a852c784ba34ca",
+			name:            "case 1: another version tag",
+			inputRef:        "v1.0.0",
+			expectedVersion: "1.0.0",
 		},
 		{
-			name:            "case 2: tagged version",
-			inputRef:        "d1dcd7e42b044858f14ad51ea68e2809c16deb84",
+			name:            "case 2: tagged commit",
+			inputRef:        "d589fbd97ae6f96325df5fbb16a1a5c7b7c7cce5",
 			expectedVersion: "0.1.0",
 		},
 		{
-			name:            "case 3: above tagged version",
-			inputRef:        "b62b39c5f762eae26979715599a0a9226547ef5e",
-			expectedVersion: "0.1.0-b62b39c5f762eae26979715599a0a9226547ef5e",
+			name:            "case 3: another tagged commit",
+			inputRef:        "06b6c8e5945ad8e7bda96e66e9c14e2052abec91",
+			expectedVersion: "1.0.0",
 		},
 		{
-			name:            "case 4: branch reference",
-			inputRef:        "test-branch",
-			expectedVersion: "0.1.0",
+			name:            "case 4: untagged commit",
+			inputRef:        "9fa9309a0275df54d61e1068f52950e794ce0f7a",
+			expectedVersion: "0.0.0-9fa9309a0275df54d61e1068f52950e794ce0f7a",
+		},
+		{
+			name:            "case 5: untagged commit with single tagged parent",
+			inputRef:        "4bff3643d8a0b413d6892ca779d8816f73061c01",
+			expectedVersion: "0.1.0-4bff3643d8a0b413d6892ca779d8816f73061c01",
+		},
+		{
+			name:            "case 6: another untagged commit with single tagged parent",
+			inputRef:        "54b81b007b74747954ba5bd5e8e75a5d039a0530",
+			expectedVersion: "1.0.0-54b81b007b74747954ba5bd5e8e75a5d039a0530",
+		},
+		{
+			name:            "case 7: untagged commit with multiple tagged parents",
+			inputRef:        "02846e1ac8dd63d7046985f6d8125241d3da81d1",
+			expectedVersion: "1.0.0-02846e1ac8dd63d7046985f6d8125241d3da81d1",
+		},
+		{
+			name:            "case 8: untagged branch with single tagged parent",
+			inputRef:        "branch-of-1.0.0",
+			expectedVersion: "1.0.0-54b81b007b74747954ba5bd5e8e75a5d039a0530",
+		},
+		{
+			name:            "case 9: untagged branch with multiple tagged parents",
+			inputRef:        "branch-of-0.1.0",
+			expectedVersion: "1.0.0-02846e1ac8dd63d7046985f6d8125241d3da81d1",
 		},
 	}
 
+	dir := "/tmp/gitrepo-test"
+	defer os.RemoveAll(dir)
+
 	c := Config{
-		Dir: "/tmp/gitrepo",
-		URL: "git@github.com:giantswarm/gitrepo.git",
+		Dir: dir,
+		URL: "git@github.com:giantswarm/gitrepo-test.git",
 	}
 	repo, err := New(c)
 	if err != nil {
@@ -56,14 +89,16 @@ func TestResolveVersion(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	for i, test := range testCases {
+	for i, tc := range testCases {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			version, err := repo.ResolveVersion(ctx, test.inputRef)
+			t.Log(tc.name)
+
+			version, err := repo.ResolveVersion(ctx, tc.inputRef)
 			if err != nil {
 				t.Errorf("%#v", err)
 			}
-			if version != test.expectedVersion {
-				t.Errorf("got %q, expected %q\n", version, test.expectedVersion)
+			if version != tc.expectedVersion {
+				t.Errorf("got %q, expected %q\n", version, tc.expectedVersion)
 			}
 		})
 	}
